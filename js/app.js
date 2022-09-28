edbf3f74f476273ec4a35198d2a01c45103b8b31
Yaroshenko-Oleg <m3coupe11@gmail.com>
2022-09-28 21:26:30 +0300

    done

---
+++ js/app.js
@@ -0,0 +1,63 @@
+$(function () {
+  var header = $("#header"),
+    introH = $("#intro").innerHeight(),
+    scrollOffset = $(window).scrollTop();
+
+  /* fixed header */
+  checkScroll(scrollOffset);
+
+  $(window).on("scroll", function () {
+    scrollOffset = $(this).scrollTop();
+    checkScroll(scrollOffset);
+  });
+
+  function checkScroll(scrollOffset) {
+    if (scrollOffset >= introH) {
+      header.addClass("fixed");
+    } else {
+      header.removeClass("fixed");
+    }
+  }
+
+  /* smooth scroll */
+  $("[data-scroll]").on("click", function (event) {
+    event.preventDefault();
+    var $this = $(this),
+      blockId = $this.data("scroll"),
+      blockOffset = $(blockId).offset().top;
+
+    $("#nav a").removeClass("active");
+    $this.addClass("active");
+
+    $("html, body").animate(
+      {
+        scrollTop: blockOffset,
+      },
+      500
+    );
+  });
+
+  /* Menu nav toggle */
+
+  $("#nav_toggle").on("click", function (event) {
+    event.preventDefault();
+    $(this).toggleClass("active");
+    $("#nav").toggleClass("active");
+  });
+
+  /* collapse*/
+  $("[data-collapse]").on("click", function (event) {
+    event.preventDefault();
+    var $this = $(this),
+      blockId = $this.data("collapse");
+    $(this).toggleClass("active");
+  });
+
+  /* slider*/
+  $("[data-slider]").slick({
+    infinite: true,
+    fade: false,
+    slidesToShow: 1,
+    slideToScroll: 1,
+  });
+});
